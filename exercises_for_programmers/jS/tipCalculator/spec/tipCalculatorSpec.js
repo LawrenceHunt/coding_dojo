80818b92a31b38d2a89ecf51b9ca29f022c03e0e
andy jenkins <andygjenkins@gmail.com>
2016-12-08 18:22:42 +0000

    Add first problem

---
+++ exercises_for_programmers/jS/tipCalculator/spec/tipCalculatorSpec.js
@@ -0,0 +1,14 @@
+describe("tipCalculator", function() {
+  var player;
+  var song;
+
+  beforeEach(function() {
+    calc = new tipCalculator();
+
+  });
+
+  it("should be able to play a Song", function() {
+
+  });
+
+});
---
+++ exercises_for_programmers/jS/tipCalculator/src/tipCalculator.js
@@ -0,0 +1,3 @@
+"use strict";
+
+// create a simple tip calculator. The program should 
