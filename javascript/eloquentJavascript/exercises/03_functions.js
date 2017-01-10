7b454db369788a2ca9ea8247e6fef8d93f69d27b
andy jenkins <andygjenkins@gmail.com>
2017-01-10 13:22:31 +0000

    Refactor isEven to handle negative

--- javascript/eloquentJavascript/exercises/03_functions.js
+++ javascript/eloquentJavascript/exercises/03_functions.js
@@ -47,14 +47,14 @@ console.log(isEven(-1));
 
 
 function isEven (num) {
-  if (num === 0)
+  var newNum = Math.abs(num);
+  if (newNum === 0)
     return true;
-  if (num === 1)
+  else if (newNum === 1)
     return false;
   else
-    return isEven(num -2);
+    return isEven(newNum -2);
 }
-
 
 
 /*
