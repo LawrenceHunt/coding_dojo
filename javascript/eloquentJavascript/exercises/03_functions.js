ba128aab405ad80dfe47c09d09537e0a2ee5e990
andy jenkins <andygjenkins@gmail.com>
2017-01-10 13:11:22 +0000

    Add isEven function soluction

--- javascript/eloquentJavascript/exercises/03_functions.js
+++ javascript/eloquentJavascript/exercises/03_functions.js
@@ -45,6 +45,18 @@ console.log(isEven(-1));
 // → ??
 */
 
+
+function isEven (num) {
+  if (num === 0)
+    return true;
+  if (num === 1)
+    return false;
+  else
+    return isEven(num -2);
+}
+
+
+
 /*
 Bean counting
 
