cf56be58ceb407b1199e11dce9667d5e6dda8af8
Christopher Sanchez <sanchez.christopherm@gmail.com>
2017-01-10 12:00:34 +0100

    Added instance variables and their tests. We have a failing test due to lack of mocking

--- exercises_for_programmers/tipCalculator/ruby/lib/tip_calculator.rb
+++ exercises_for_programmers/tipCalculator/ruby/lib/tip_calculator.rb
@@ -1,13 +1,17 @@
 class TipCalc
 
-  attr_reader :bill_total
+  attr_reader :bill_total, :tip_rate, :final_total, :tip_total
 
   def initialize
     @bill_total = 0
+    @tip_rate = 0
+    @final_total = 0
+    @tip_total = 0
   end
 
 	def bill_prompt
-		p "What is the bill?"
+		puts "What is the bill?"
+    @bill_total = collect_input
 	end
 
   def collect_input
--- exercises_for_programmers/tipCalculator/ruby/spec/tip_calc_spec.rb
+++ exercises_for_programmers/tipCalculator/ruby/spec/tip_calc_spec.rb
@@ -3,15 +3,37 @@ require 'spec_helper'
 describe TipCalc do
 	subject(:tip_calc) {described_class.new()}
 
+	describe "#initialize" do
+		it "should initialize bill_total at zero " do
+			expect(tip_calc.bill_total).to eq(0)
+		end
+
+		it "should initialize tip_total at zero " do
+			expect(tip_calc.tip_total).to eq(0)
+		end
+
+		it "should initialize tip_rate at zero " do
+			expect(tip_calc.tip_rate).to eq(0)
+		end
+
+		it "should initialize final_total at zero " do
+			expect(tip_calc.final_total).to eq(0)
+		end
+	end
+
 	describe "#bill_prompt" do
 		it 'prompts the user to input the bill' do
-			expect(tip_calc.bill_prompt).to eq("What is the bill?")
+			#STDOUT.should_receive(:puts).with("What is the bill?")
+			allow(tip_calc.bill_prompt).to receive(:collect_input) { "200\n" }
+			expect(STDOUT).to receive(:puts) {"What is the bill?"}
+			tip_calc.bill_prompt
 		end
 	end
 
 	describe "#collect_input" do
 		it 'collect user input' do
-			allow(tip_calc).to receive(:gets) { "200\n" }
+			allow(tip_calc.collect_input).to receive(:gets) { "200\n" }
+			#tip_calc.bill_prompt
 			expect(tip_calc.bill_total).to eq "200"
 		end
 	end
