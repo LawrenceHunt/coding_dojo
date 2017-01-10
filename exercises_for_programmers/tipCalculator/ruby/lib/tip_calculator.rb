afac13b9fa9d29effd14401f6c2c99d493cec687
Christopher Sanchez <sanchez.christopherm@gmail.com>
2017-01-10 10:43:10 +0100

    Added tests for collect input

--- exercises_for_programmers/tipCalculator/ruby/lib/tip_calculator.rb
+++ exercises_for_programmers/tipCalculator/ruby/lib/tip_calculator.rb
@@ -1,5 +1,7 @@
 class TipCalc
 
-
+	def bill_prompt
+		p "What is the bill?"
+	end
 
 end
--- exercises_for_programmers/tipCalculator/ruby/spec/tip_calc_spec.rb
+++ exercises_for_programmers/tipCalculator/ruby/spec/tip_calc_spec.rb
@@ -1,9 +1,28 @@
 require 'spec_helper'
 
 describe TipCalc do
+	subject(:tip_calc) {described_class.new()}
 
+	describe "#bill_prompt" do
+		it 'prompts the user to input the bill' do
+			expect(tip_calc.bill_prompt).to eq("What is the bill?")
+		end
+	end
 
+	describe "#collect_input" do
+		before do
+			$stdin = StringIO.new("200")
+		end
 
+		after do
+			$stdin = STDIN
+		end
 
+		it 'collect user input' do
+			tip_calc.collect_input
+			expect(tip_calc.collect_input).to be == "200"
+		end
+	end
 
 end
+
