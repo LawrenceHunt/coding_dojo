977094822abf0400492c20845f147f128238c658
Christopher Sanchez <sanchez.christopherm@gmail.com>
2017-01-11 11:53:31 +0100

    Added new test for calculating the final total

--- exercises_for_programmers/tipCalculator/ruby/cs_solution/spec/tip_calc_spec.rb
+++ exercises_for_programmers/tipCalculator/ruby/cs_solution/spec/tip_calc_spec.rb
@@ -43,16 +43,28 @@ describe TipCalc do
 
 	describe "#just_the_tip_plz" do
 		before(:example) do
-
-			# allow(tip_calc).to receive(:bill_total) { "200" }
-			# allow(tip_calc).to receive(:tip_rate) { "15" }
+			tip_calc.instance_variable_set(:@bill_total, "200")
+			tip_calc.instance_variable_set(:@tip_rate, "15")
 		end
 
 		it 'calculates the tip' do
-			tip_calc = double("Anything", :bill_total => "200", :tip_rate => "15")
 			tip_calc.just_the_tip_plz
 			expect(tip_calc.tip_total).to eq(30)
 		end
+
+	end
+
+	describe "#final_total" do
+	 	before(:example) do
+			tip_calc.instance_variable_set(:@bill_total, "200")
+			tip_calc.instance_variable_set(:@tip_total, 30)
+		end
+
+		it 'calculates the final' do
+			tip_calc.final_total
+			expect(tip_calc.final_total).to eq(230)
+		end
+
 	end
 
 end
