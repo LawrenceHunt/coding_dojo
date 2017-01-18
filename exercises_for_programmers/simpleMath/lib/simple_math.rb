ab4eec4ad52897d0e6eb538007bab160c9996ef2
andy jenkins <andygjenkins@gmail.com>
2017-01-18 09:30:33 +0000

    Pass prompt test

--- exercises_for_programmers/simpleMath/lib/simple_math.rb
+++ exercises_for_programmers/simpleMath/lib/simple_math.rb
@@ -6,4 +6,12 @@ class SimpleMath
 		@first_num = 0
 		@sec_num = 0
 	end
+
+	def num_prompt
+		puts "What is the first number?"
+		@first_num = gets.chomp
+		puts "What is the second number?"
+		@sec_num = gets.chomp\
+	end
+
 end
